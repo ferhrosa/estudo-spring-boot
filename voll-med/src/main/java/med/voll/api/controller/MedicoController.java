87a793a68c1fca1ff2a2415db89df494fa1c7b76
Fernando H. Rosa <ferhrosa@gmail.com>
2023-04-08 19:19:03 +0000

    Listagem de médicos e pacientes
    Também foi adicionada configuração do JPA para mostrar cada SQL sendo exeuctado (e formatado).

--- voll-med/src/main/java/med/voll/api/controller/MedicoController.java
+++ voll-med/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 
@@ -18,6 +23,11 @@ public class MedicoController {
 
     @Autowired
     private MedicoRepository medicoRepository;
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(@PageableDefault(size = 3, sort = { "nome" }) Pageable paginacao) {
+        return medicoRepository.findAll(paginacao).map(DadosListagemMedico::new);
+    }
 
     @PostMapping
     @Transactional
