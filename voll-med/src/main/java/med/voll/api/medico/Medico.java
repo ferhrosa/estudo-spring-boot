f509c2fdb854d1f095e1ea2e9bdde77bc01ce897
Fernando H. Rosa <ferhrosa@gmail.com>
2023-04-08 17:46:08 +0000

    Campo de telefone no cadastro de médicos

--- voll-med/src/main/java/med/voll/api/medico/Medico.java
+++ voll-med/src/main/java/med/voll/api/medico/Medico.java
@@ -27,6 +27,7 @@ public class Medico {
     private Long id;
     private String nome;
     private String email;
+    private String telefone;
     private String crm;
 
     @Enumerated(EnumType.STRING)
@@ -36,10 +37,12 @@ public class Medico {
     private Endereco endereco;
 
     public Medico(DadosCadastroMedico dados) {
-        this.nome = dados.nome();
-        this.email = dados.email();
-        this.crm = dados.crm();
-        this.especialidade = dados.especialidade();
-        this.endereco = new Endereco(dados.endereco());
+        this(null,
+                dados.nome(),
+                dados.email(),
+                dados.telefone(),
+                dados.crm(),
+                dados.especialidade(),
+                new Endereco(dados.endereco()));
     }
 }
